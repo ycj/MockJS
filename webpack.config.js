544adf0a3fa1433e22ee0c2c4c32f78e0161abad
ycj <yuanchangjun@gmail.com>
2023-11-15 08:52:14 +0800

    first test

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/mock.js',
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'mock-min.js',
+    filename: 'mock.js',
     library: 'Mock',
     libraryTarget: 'umd',
     globalObject: 'this',
